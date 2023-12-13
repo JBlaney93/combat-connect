0beaa3840d0b42085c422f5e238c957a26d590a9
James Blaney <jamesblaney93@gmail.com>
2023-12-13 21:06:07 +0000

    small db change

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -4,6 +4,8 @@ export const users = sqliteTable('users', {
     id: integer('id').primaryKey(),
     firstName: text('first_name'),
     lastName: text('last_name'),
+    nickName: text('nick_name'),
+    weightClass: text('weight_class'),
   }
 );
 
---
+++ drizzle/0002_orange_cyclops.sql
@@ -0,0 +1,2 @@
+ALTER TABLE users ADD `nick_name` text;--> statement-breakpoint
+ALTER TABLE users ADD `weight_class` text;
